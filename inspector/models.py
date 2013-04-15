cc09124548e15b7d4e6eff3185750f092558a9e6
Daniele Procida <daniele@vurt.org>
2013-04-15 16:33:03 +0100

    moved things around a bit; fewer database hits

--- inspector/models.py
+++ inspector/models.py
@@ -13,25 +13,39 @@ class Inspection(models.Model):
     
     def start_inspection(self):
         self.save()
-        path = Path(
-            inspection=self,
-            path=self.start_path
-            )     
-        
-        # seed it with the first path
-        uninspected_paths = [path]
+            
+        uninspected_paths = [Path(inspection=self, path=self.start_path) ]
+        discovered_hrefs = set()
         
         # are there any uninspected pages left?
         while uninspected_paths:
 
+            path = uninspected_paths[0]
             # check it for a response and further paths
-            uninspected_paths[0].inspect()
+            response = path.inspect()
+
+            # we are only interested in HTML documents 
+            if "text/html" in path.content_type:
+
+                # examine the page for new hrefs
+                soup = BeautifulSoup(''.join(response.content)) 
+
+                # for each anchor href that starts with / and is not already discovered
+                for href in [anchor["href"] for anchor in soup.findAll('a', href=True) if anchor["href"].startswith("/") and not anchor["href"] in discovered_hrefs]: 
+                
+                    discovered_hrefs.add(href)
+                
+                    Path(
+                        inspection=self,
+                        path=href, 
+                        referrer=path
+                        ).save()
 
             # find uninspected_paths
             uninspected_paths = self.paths.filter(response_code=None)
              
             print self.paths.filter().count(), 
-            print "found, or which", uninspected_paths.count(), "remain to be inspected"
+            print "found, of which", uninspected_paths.count(), "remain to be inspected"
         
 
 class Path(models.Model):
@@ -60,20 +74,4 @@ class Path(models.Model):
         self.content_type = response.get("content-type", "")
         self.save()
        
-        # we are only interested in HTML documents 
-        if "text/html" in self.content_type:
-
-            # examine the page for new hrefs
-            soup = BeautifulSoup(''.join(response.content)) 
-
-            # make a list of hrefs
-            hrefs = [path.path for path in self.inspection.paths.all()]
-
-            # for each anchor href that starts with / and is not in hrefs already
-            for href in [anchor["href"] for anchor in soup.findAll('a', href=True) if anchor["href"].startswith("/") and not anchor["href"] in hrefs]: 
-                
-                path = Path(
-                    inspection=self.inspection,
-                    path=href, 
-                    referrer=self
-                    ).save()
+        return response
